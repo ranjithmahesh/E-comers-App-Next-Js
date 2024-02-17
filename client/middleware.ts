491ea089e86f9d10cf92809aa8e807d2e96ea69f
ranjithmahesh <ranjithmahesh1997@gmail.com>
2024-02-17 22:43:25 +0530

    z

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // publicRoutes: ["/", "/project-deatils/(.*)"],
+   publicRoutes: ["/", "/project-deatils/(.*)"],
 });
 
 export const config = {
